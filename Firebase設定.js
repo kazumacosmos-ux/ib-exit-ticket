f02b91e15b3a52aca504dc5003dc719c41710f10
kazumacosmos-ux <kazuma.cosmos@gmail.com>
2026-09-08 22:15:28 +0900

    Update Firebase設定.js

--- Firebase設定.js
+++ Firebase設定.js
@@ -1,2 +1,9 @@
-// 現在使っている firebase-confine.js の内容を、このファイルにコピーしてください。
-// 形式は export const firebaseConfig = { ... }; です。
+export const firebaseConfig = {
+  apiKey: "AIzaSyA6xBMX14Z4gO4KE8Dy0qtTrQayDh9Fuzc",
+  authDomain: "bdeguchi-1666f.firebaseapp.com",
+  databaseURL: "https://bdeguchi-1666f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bdeguchi-1666f",
+  storageBucket: "bdeguchi-1666f.firebasestorage.app",
+  messagingSenderId: "578146539417",
+  appId: "1:578146539417:web:159ff465f65a3fdfeea508"
+};
