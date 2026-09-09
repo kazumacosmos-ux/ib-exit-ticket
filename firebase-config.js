d48dd00fd46411c1d491b0422e647001b75f1f69
kazumacosmos-ux <kazuma.cosmos@gmail.com>
2026-09-10 08:24:27 +0900

    firebase-config.js の削除

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA6xBMX14Z4gO4KE8Dy0qtTrQayDh9Fuzc",
-  authDomain: "bdeguchi-1666f.firebaseapp.com",
-  databaseURL: "https://bdeguchi-1666f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bdeguchi-1666f",
-  storageBucket: "bdeguchi-1666f.firebasestorage.app",
-  messagingSenderId: "578146539417",
-  appId: "1:578146539417:web:159ff465f65a3fdfeea508"
-};
